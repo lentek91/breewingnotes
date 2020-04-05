fae792b8f09bef38974025ff5a6ef0c91b11c98d
Lentek <klentowicz91@gmail.com>
2020-04-05 15:14:29 +0200

    Implementacja

--- application/src/main/java/pl/lentek/brewingnotes/BrewingNotesRunner.java
+++ application/src/main/java/pl/lentek/brewingnotes/BrewingNotesRunner.java
@@ -1,5 +1,6 @@
 package pl.lentek.brewingnotes;
 //
+//
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,8 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication//
 public class BrewingNotesRunner {
 //
-  public static void main(String[] args) {
+  public static void main(String[] args) {//
     SpringApplication.run(BrewingNotesRunner.class, args);
-  }
+  }//
 //
 }//
